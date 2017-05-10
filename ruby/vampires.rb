889e6a6b0ab7a10bba7e651677163260a840c609
mpletcher <pletcher.marcos@icloud.com>
2017-05-10 17:37:42 -0400

    Declare while loop to ask user for possible allergies

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,7 +5,7 @@ Email: pletcher.marcos@icloud.com
 =end
 
 puts "How many employees will be processed?"
-number_employees = gets.chomp.t_i #stores the number of employees will be processed
+number_employees = gets.chomp.to_i #stores the number of employees will be processed
 
 number_turns = 0; # stores number of cycles; it starts at zero
 
@@ -14,17 +14,43 @@ while number_turns < number_employees
 	employee_name = gets.chomp.capitalize # capitalizes only the first letter of a string
 
 	puts "How old are you?"
-	age = gets.chomp.t_i 
+	age = gets.chomp.to_i
 
 	puts "What year were you born?"
-	year = gets.chomp.t_i 
-
+	year = gets.chomp.to_i
+	
 	puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y or N)"
 	galic_food = gets.chomp.downcase # makes every letter in a string lowercase
 
 	puts "Would you like to enroll in the company’s health insurance? (Y or N)"
 	health_insurance = gets.chomp.downcase # makes every letter in a string lowercase
+
+	# Discover the correct age
+	correct_age = 2017 - year
+
+		# Checks for Suspicious Allergies
+		allergies = "" # Declare variable 
+		while allergies != "done" || allergies != "sunshine"
+			puts "Let us know please if you are allergic to anything. Additionally, type “done” when finished."
+			allergies = gets.chomp
+
+			if allergies == "sunshine"
+				puts "Probably a vampire."
+			elsif age < 120 && (health_insurance == "yes" || galic_food == "yes")
+				puts "Probably not a vampire."
+			elsif age > 120 && (health_insurance == "no" || galic_food == "no")
+				puts "Probably a vampire."
+			elsif age != correct_age && health_insurance == "no" || galic_food == "no"
+				puts "Almost certainly a vampire."
+			elsif employee_name == "Drake Cula" || employee_name == "Tu Fang"
+				puts "Definitely a vampire."
+			else
+	    	puts "Results inconclusive."
+			end 
+		end
+	num_cycles += 1
 end
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
 
 
