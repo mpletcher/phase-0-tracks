26ddb16ee419448c0716752e76eac286fddf545c
mpletcher <pletcher.marcos@icloud.com>
2017-05-16 15:57:19 -0400

    Initial commit and add Pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,13 @@
+=begin
+5.2 Arrays and Hashes
+Author: Marcos Pletcher
+
+-------------Pseudocode -----------------
+Prompt the user for his/her name, age, number of children, decor theme, 
+If user inputs unvalid data, re-promt the user
+Convert any user input to the appropriate data type
+Prompt the user if any information needs to be changed
+Prompt the user to enter the new value
+Otherwise, print last information
+=end
+
