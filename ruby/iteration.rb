3267ac82ef537053f51cfd175c3b121705002e12
mpletcher <pletcher.marcos@icloud.com>
2017-05-18 17:21:46 -0400

    Declare each, map, and map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -12,11 +12,41 @@ Author: Marcos Pletcher, Tyler Kawabata
 # Write Your Own Method That Takes a Block
 # print out a status message before and after running the block
 # ---------------------------------
+def ask_for_name
+  puts "What's your name?"
+  user_name = gets.chomp.capitalize
+  puts "\nName entered: #{user_name}"
+  yield
+  #block  
+end
+ask_for_name {puts "Nice to meet you!"}
 
 # ---------------------------------
 # Release 1: Use each, map, and map!
 # ---------------------------------
+# each
+puts "\n ------------------"
+# A list of names
+names = [ "Fred", "Tiago", "Jim" ]
 
+names.each do|l|
+  p "Hello #{l}"
+end
+
+# map (items need to be enumerable)
+puts "\n ------------------"
+array = []
+array.push(1,2,3)
+
+array.map { |n| n * n } # [1, 4, 9]
+p array
+
+# map! (Change the original array)
+puts "\n ------------------"
+names = ["marcos", "tom"]
+
+names.map! {|item| item.capitalize} # ["Marcos", "Tim", "Chris"]
+p names
 # ---------------------------------
 # Release 2: Use the Documentation (Do the thing)
 # ---------------------------------
