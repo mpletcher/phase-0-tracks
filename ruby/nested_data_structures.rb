9c7227776f15f7aad3bfefeb4a3f1ad5a99ca7fa
mpletcher <pletcher.marcos@icloud.com>
2017-05-17 23:54:04 -0400

    Declare nested arrays and hashes

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,309 @@
+=begin
+5.4 Nested Data Structures
+Author: Marcos Pletcher
+=end
+
+# Design and Build a Nested Data Structure
+
+nyc_midtown_neighborhoods  = {
+    midtown: {
+
+    },
+    midtown_west: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+
+  },
+    midtown_east: {
+         # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]   
+
+  },
+    columbus_circle: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    sutton_place: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    rockefeller_center: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    diamond_district: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    theater_district: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    turtle_bay: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    midtown_east: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    tudor_city: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    little_brazil: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    times_square: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    hudson_yards: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    midtown_west: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    hells_kitchen: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  }
+    garment_district: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    herald_square: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    koreatown: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    murray_hill: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    tenderloin: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  },
+    madison_square: {
+        # distinct data regarding this neighborhood
+        information: {
+            landmark:,
+            rent_average:,
+            limits_south_north_east_west:,
+
+
+        }
+        # list of movies
+        appears_in_a_number_of_movies: [
+
+    ]
+  }
+} # close hash
+
