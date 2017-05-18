ceaa7b0e395fe921ad2c7d095e53216669c3ebfc
mpletcher <pletcher.marcos@icloud.com>
2017-05-18 17:11:46 -0400

    initial commit

---
+++ ruby/iteration.rb
@@ -0,0 +1,29 @@
+=begin
+5.3 Mandatory Pairing: Iteration
+Author: Marcos Pletcher, Tyler Kawabata
+=end
+
+# ---------------------------------
+# Release 0: Explore Blocks
+# Use the .times Method
+# ---------------------------------
+2.times { p "Welcome!"}
+# ---------------------------------
+# Write Your Own Method That Takes a Block
+# print out a status message before and after running the block
+# ---------------------------------
+
+# ---------------------------------
+# Release 1: Use each, map, and map!
+# ---------------------------------
+
+# ---------------------------------
+# Release 2: Use the Documentation (Do the thing)
+# ---------------------------------
+
+# Declare Array and Hash
+
+# 1. A method that iterates through the items, deleting anything
+# 2. A method that filters a data structure for only items that do satisfy a certain condition
+# 3. A different method that filters a data structure for only items satisfying a certain condition 
+# 4. A method that will remove items from a data structure until the condition in the block evaluates to false, then stops
