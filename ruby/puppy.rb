cbb1f20b3f29190508b55a17b4a268991f676a84
mpletcher <pletcher.marcos@icloud.com>
2017-05-23 13:25:30 -0400

    Initial commit

---
+++ ruby/puppy.rb
@@ -0,0 +1,23 @@
+=begin
+6.1 Introduction to Classes
+Author: Marcos Pletcher
+
+
+SPECIES ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Name: varies
+Trait: varies
+Color: varies
+age: varies
+
+BEHAVIOR -----------------------
+Jump
+Dig
+Lick
+Eat
+Bark
+Sleep
+
+=end
