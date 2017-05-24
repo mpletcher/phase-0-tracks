e49cb12ff9762d12893d8f04dcb5227986aca1c8
mpletcher <pletcher.marcos@icloud.com>
2017-05-24 19:32:17 -0400

    Create our own class and declared required instances

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -7,6 +7,7 @@ Author: Marcos Pletcher, Erik Wotring
 # Release 1: Declare an Initialize Method
 # -----------------------------------
 
+# constant
 DOG_YEARS = 7
 
 class Puppy
@@ -21,6 +22,7 @@ class Puppy
   end # close fetch
 
   # Add a speak method that takes an integer, and then prints "Woof!" that many times.
+  # it takes an integer as parameter and prints woof that integer times
   def speak(barking_sound)
 
     # block
@@ -34,11 +36,17 @@ class Puppy
 
   # Add a dog_years method that takes an integer (of human years) and converts that number to dog years, returning a new integer.
   # one human year is 7 dog years
-
+  def dog_years(age)
+    p age.to_i * DOG_YEARS
+  end
 
   # add a trick
+  def shake_hands
+    p "\nDog raises paw\n"
+  end
 
 end # close main method
+
 
 # -----------------------------------
 # Object creates puppies
@@ -47,8 +55,90 @@ puppy =Puppy.new
 # call class
 puppy.fetch('ball')
 puppy.speak(8)
+puppy.roll_over
+puppy.dog_years(6)
+puppy.shake_hands
 
 # -----------------------------------
 # Release 2: Write Your Own Class, and Experiment!
 # -----------------------------------
+# a class to classify wizards
+# a constructor initialize holds two instance variables for our wizards
+# constants of magic
+
+LEVEL1 = "Physical sensitivity to subtle energy."
+LEVEL2 = "Extra physical sensitivity to subtle energy."
+LEVEL3 = "Extra-dimensional awareness."
+
+# Global variables
+$greetings = puts "Welcome!\n\n"
+
+# create a class
+class Wizards
+  # constructor initialize
+  def initialize
+    puts "Let's create our list of powerful wizards.\n\n"
+  end
+
+
+  # loop 50 times
+  array = []
+
+  50.times do
+    wizards = Wizards.new
+    array << wizards
+  end
+
+  # .each
+  def magical_powers
+    list_of_powers = ['telepathy','teleportation','levitation','healing']
+
+    list_of_powers.each do |item|
+      p "List of Powers: #{item}!"
+    end
+  end
+
+  # change normal name to secret name
+  def increasing_powers
+
+    list_of_powers = ['telepathy','teleportation','levitation','healing']
+
+    list_of_powers.map! do |item|
+      'Increasing ' + item + ' power. '
+    end
+    p list_of_powers.join
+  end
+
+  def inspect_power(experience)
+
+    case experience 
+      when 0 .. 20
+        puts "\n#{experience} years - your level of power is: #{LEVEL1}\n\n"
+      when 21 .. 70
+        puts "\n#{experience} years - your level of power is: #{LEVEL2}\n\n"
+      when 71 .. 200
+        puts "\n#{experience} years - your level of power is: #{LEVEL3}\n\n"
+      else
+        puts "\nThis program cannot identify your level of power.\n\n"
+    end
+
+  end # ends inspect
+
+
+end # ends method
+
+# dont have to re-type the whole thing
+$greetings
+
+# Create wizard objects
+wizard = Wizards.new
+
+
+# call class
+wizard
+wizard.magical_powers
+wizard.inspect_power(20)
+wizard.increasing_powers
+
+
 
