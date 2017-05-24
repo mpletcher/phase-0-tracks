7fea3e7d758bde9652393f43d15e175330e47d6d
mpletcher <pletcher.marcos@icloud.com>
2017-05-24 18:05:37 -0400

    Initial commit

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,48 @@
-class Puppy
+=begin
+6.1 Introduction to Classes
+Author: Marcos Pletcher, Erik Wotring
+=end
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+# -----------------------------------
+# Release 1: Declare an Initialize Method
+# -----------------------------------
+
+DOG_YEARS = 7
+
+class Puppy
+  # class method
+  def initialize()
+    puts "Initializing new puppy instance ...\n\n"
   end
 
-end
+  def fetch(toy)
+    puts "I brought back the #{toy}!\n\n"
+    toy
+  end # close fetch
+
+  # Add a speak method that takes an integer, and then prints "Woof!" that many times.
+
+
+  # Add a roll_over method that just prints "*rolls over*".
+
+
+  # Add a dog_years method that takes an integer (of human years) and converts that number to dog years, returning a new integer.
+  # one human year is 7 dog years
+
+
+  # add a trick
+
+end # close main method
+
+# -----------------------------------
+# Object creates puppies
+puppy =Puppy.new
+
+# call class
+#puppy.fetch('ball')
+
+
+# -----------------------------------
+# Release 2: Write Your Own Class, and Experiment!
+# -----------------------------------
 
