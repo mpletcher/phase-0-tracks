aef749092535e3f144b84e23a983b3c93ab81a7f
mpletcher <pletcher.marcos@icloud.com>
2017-05-24 18:27:06 -0400

    Declare fetch, speak, and roll_over methods for this release 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -21,10 +21,16 @@ class Puppy
   end # close fetch
 
   # Add a speak method that takes an integer, and then prints "Woof!" that many times.
+  def speak(barking_sound)
 
+    # block
+    barking_sound.to_i.times {|barking_sound| p "Woof!"}
+  end
 
   # Add a roll_over method that just prints "*rolls over*".
-
+  def roll_over
+    puts "\n*rolls over*\n"
+  end
 
   # Add a dog_years method that takes an integer (of human years) and converts that number to dog years, returning a new integer.
   # one human year is 7 dog years
@@ -39,8 +45,8 @@ end # close main method
 puppy =Puppy.new
 
 # call class
-#puppy.fetch('ball')
-
+puppy.fetch('ball')
+puppy.speak(8)
 
 # -----------------------------------
 # Release 2: Write Your Own Class, and Experiment!
