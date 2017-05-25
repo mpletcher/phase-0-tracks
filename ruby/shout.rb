6385669c4cf10efd0ccc930806f73ce5b3fca3fc
mpletcher <pletcher.marcos@icloud.com>
2017-05-25 13:34:18 -0400

    Initial commit

---
+++ ruby/shout.rb
@@ -0,0 +1,13 @@
+=begin
+6.4 Modules
+Author: Marcos Pletcher
+=end
+
+# -----------------------
+# Release 1: Write a Simple Module
+# -----------------------
+
+# declare a module
+module Shout
+  # we'll put some methods here soon, but this code is fine for now!
+end
