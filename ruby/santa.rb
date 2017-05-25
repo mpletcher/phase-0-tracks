614df7c31a893ea6d30dafaaaaaf9988d6bbb5a6
mpletcher <pletcher.marcos@icloud.com>
2017-05-24 21:20:43 -0400

    Declare attributes for this class, create instant variable

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,10 +5,19 @@ Author: Marcos Pletcher
 
 # Add state to classes
 class SantaCon
-  
+
+  attr_reader :ethnicity
+  attr_accessor :age, :gender
+
   # Constructor
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
+
+    # instance variables
+    @age        = 0
+    @gender     = gender
+    @ethnicity  = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
 
   def speak
@@ -19,14 +28,25 @@ class SantaCon
     puts "\nThat was a good #{snickerdoodle}!"
   end
 
+  # age Santa by one year.
+  def celebrate_birthday
+    puts "Happy birthday Santa!"
+    @age +=1
+  end
+
 end
 
 
 # Driver code
 # instantiating
-santa = SantaCon.new
+santa = SantaCon.new("male", "white")
 santa.speak
 santa.eat_milk_and_cookies("ginger cookie")
 
 # call class
 SantaCon
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "N/A"]
+
+
