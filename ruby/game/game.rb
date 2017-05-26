537783f163ff1c087b9db2140b2c9a10338dc0a1
mpletcher <pletcher.marcos@icloud.com>
2017-05-26 18:33:56 -0400

    First commit

---
+++ ruby/game/game.rb
@@ -0,0 +1,8 @@
+=begin
+6.7 Solo Challenge: A Game Class
+Author: Marcos Pletcher
+=end
+
+# game class
+class Game
+end 
---
+++ ruby/game/game_spec.rb
@@ -0,0 +1,6 @@
+=begin
+6.7 Solo Challenge: A Game Class
+Author: Marcos Pletcher
+=end
+
+require_relative 'game'
