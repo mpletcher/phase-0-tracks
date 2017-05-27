445cd9130249e16c921215334c85d233a092d25f
mpletcher <pletcher.marcos@icloud.com>
2017-05-26 23:55:50 -0400

    Declare initialize constructor, add user interface messages, and check instance methods of this game

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -6,8 +6,14 @@ Author: Marcos Pletcher
 game class
   # att accessor
   # make input readable/ writable
+ 	  attr_accessor :entered_word, :your_attempt
+  
 
   # define initialize constructor
+    def initialize(items)
+      @entered_word = items.downcase.split('')
+      @attempts = 0
+    end
   # pass an argument as parameter
   # create instance variables for entered content and attempts
 
@@ -19,7 +25,10 @@ game class
 # user interface
   # prints greetings
   # prompt user to enter something
-  #
+	puts "Let's Play! (This game is better with 2 players!)"
+	puts "Enter a word, then your friend needs to tell us what word is that"
+	entered_word = gets.chomp
+	game = Game.new(entered_word)
   # WHILE attempts are less than all attempts
   # show quantity of attempts and ask another player for letting us know what  thing you typed
   # IF his/her attempt is correct, say "Great! Good job.. you can read minds"
