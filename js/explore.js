95c24101c4f4f1c4cbadd7cc197b873f25fd5a5a
mpletcher <pletcher.marcos@icloud.com>
2017-05-30 17:24:28 -0400

    Define function that reverses string and simple IF ELSE statement

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,8 @@
+/*
+7.1 Syntax
+Author: Marcos Pletcher
+*/
+
 // Pseudocode
 // define a function 
 // this function takes one parameter (string) and reverse it
@@ -6,3 +11,26 @@
 // for each letter in the string, shovel content into empty variable
 // print the reversed string
 // IF hello is different that reversed string, say something
+
+function reverseString(string) {
+
+    var reversed_string = '';
+
+    for (var string_length = string.length - 1; string_length >= 0; string_length -= 1) {
+        reversed_string += string[string_length];
+    }
+
+    console.log(reversed_string);
+    
+    if (reversed_string == 'socraM si eman yM') {
+      console.log("\nCorrect!");
+    }
+    else {
+      console.log("\nFalse!"); 
+    }
+}
+
+
+// Driver code
+//reverseString('My name is Marcos');  -- output: "socraM si eman yM"
+reverseString('Marcos')
