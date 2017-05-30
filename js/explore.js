694ad56e4d00e2518748a3a9c88725a828741027
mpletcher <pletcher.marcos@icloud.com>
2017-05-30 17:20:03 -0400

    Initial commit

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// Pseudocode
+// define a function 
+// this function takes one parameter (string) and reverse it
+// loop over the variable counting its length
+// create a new empty variable
+// for each letter in the string, shovel content into empty variable
+// print the reversed string
+// IF hello is different that reversed string, say something
