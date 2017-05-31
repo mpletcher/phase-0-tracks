9dcff6a9754dbcf6d100ebad870d61dd0e8a57e2
mpletcher <pletcher.marcos@icloud.com>
2017-05-31 19:15:52 -0400

    First commit

---
+++ js/data_structures.js
@@ -0,0 +1,34 @@
+/*
+Release 0: Work with Arrays
+*/
+
+// set an array of colors
+var colors = ["blue", "pink", "mauve", "tourquoise"]
+
+// set and array of four names
+var names = ["Usagi", "Ami", "Rei", "Makoto"]
+
+// Write additional code that adds another color to the colors array, 
+// and another horse name to the horses array
+colors.push("Cerulean")
+
+names.push("Tuxedo Mask")
+
+/*
+Release 1: Build an Object
+*/
+
+// declare an empty object
+// assign colors to horses
+
+	// create a for loop that starts counting a temporary variable from 0, iterates over the the length of names
+
+
+
+/*
+Release 2: Build Many Objects Using a Constructor
+*/
+
+// Drive code
+console.log(colors);
+console.log(names);
