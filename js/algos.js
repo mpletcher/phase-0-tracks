3cf2f4474da6c220d6ffc3301fd9c38df150b5a2
mpletcher <pletcher.marcos@icloud.com>
2017-06-03 12:30:20 -0400

    Declare function, iterate over each item of array, return the highest value

--- js/algos.js
+++ js/algos.js
@@ -2,3 +2,27 @@
 7.3 Solo Challenge: Algorithm Practice
 Author: Marcos Pletcher
 */
+
+/*
+------------------------------------------
+Release 0: Find the Longest Phrase
+------------------------------------------
+Write a function that takes an array fo strings and returns the longest value.
+Iterate over each items in the array and find its length
+Store the lengths of each items to use for comparison
+return the highest value
+ */
+function bigString(array) {
+    var longestThing = "";
+
+    for (var x = 0; x < array.length; x++ ) {
+        if (array[x].length > longestThing.length) {
+            longestThing = array[x];
+        }
+    }
+    return longestThing;
+}
+
+
+// Driver code
+//console.log(bigString(["Honda", "Yuki", "Kyou", "Shigure", "Hatsuharu", "Akito", "Akio", "Hatori", "Ayame", "Fruits Basket"]))
