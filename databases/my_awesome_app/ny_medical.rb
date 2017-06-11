ec5d969f61c1ba7e2241963480985fc3aec35098
mpletcher <pletcher.marcos@icloud.com>
2017-06-10 22:40:53 -0400

    Make tables and set attritutes

--- databases/my_awesome_app/ny_medical.rb
+++ databases/my_awesome_app/ny_medical.rb
@@ -24,10 +24,46 @@ db = SQLite3::Database.new("NYMedical.db")
 db.results_as_hash = true
 
 # Make a new table
+CENTER_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS CENTER(
+		center_id INTEGER PRIMARY KEY,
+		street_address VARCHAR(100), 
+		city VARCHAR(100),    
+		state_abbr CHAR(2),
+		phone_number CHAR(12)  
+	)
+SQL
 
 # Make a new table
+EMPLOYEE_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS EMPLOYEE(
+		id INTEGER PRIMARY KEY,
+		first_name VARCHAR(50),
+		last_name VARCHAR(50),
+		ssn VARCHAR(11),
+		gender CHAR(1),
+		street_address VARCHAR(100),
+		city VARCHAR(100),
+		state_abbr CHAR(2),
+		zip CHAR(10),
+		phone_number CHAR(12),
+		title VARCHAR(100),
+		email CHAR(10), 
+		years INT,
+		center_ref INT,
+		FOREIGN KEY(center_ref) REFERENCES CENTER(center_id)
+	)
+SQL
 
 # Make a new table
+HR_EMPLOYEE_RETENTION_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS HR_EMPLOYEE_RETENTION(
+		rate INT,
+		employee_id INT,	
+		FOREIGN KEY(employee_id) REFERENCES EMPLOYEE(id)    
+	)
+SQL
+
 
 # Create a table (if it's not there already)
 
