2f596af212ac81c701287a1e89dc3607769049d4
mpletcher <pletcher.marcos@icloud.com>
2017-06-13 18:26:32 -0400

    Fix event in JS

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,9 +1,13 @@
+// Release 1 
 var els = document.getElementsByTagName("h3");
 var el = els[0];
 
+// Release 2
 function addRadiosProperty() {
     el.style.border = "2px solid #ccc";
+    console.log("Testing my JS!");
 
 }
 
-el.addEventListener("hover", addRadiosProperty);
+el.addEventListener("click", addRadiosProperty);
+
