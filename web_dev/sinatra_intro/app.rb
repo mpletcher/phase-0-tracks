6c203e11a9ab5c72629ae52fdfaf4cbf8b2a2991
mpletcher <pletcher.marcos@icloud.com>
2017-06-14 11:10:24 -0400

    Method takes a person's name as a query parameter

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -21,15 +21,6 @@ get '/about/:person' do
   "#{person} is a programmer, and #{person} is learning Sinatra."
 end
 
-# route parameters
-get '/contact' do
-	"Test"
-end
-
-get '/:person_1/loves/:person_2' do
-  "#{params[:person_1]} loves #{params[:person_2]}"
-end
-
 # write a GET route that retrieves
 # all student data
 get '/students' do
@@ -50,4 +41,25 @@ end
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
+end
+
+# route parameters
+# pass information via params
+get '/contact' do
+	"The address of the Tokyo Tower is: Japan, 〒105-0011 Tokyo, Minato, Shibakoen, 4 Chome−２−８- <a href='https://goo.gl/AtxFBs'> Open it on GoogleMaps</a>"
+end
+
+# A /contact route that displays an address (you can make up the address)
+get '/:person_1/loves/:person_2' do
+  "#{params[:person_1]} loves #{params[:person_2]}"
+end
+
+# A /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". 
+#If the query parameter is not present, the route simply says "Good job!"
+get '/great_job/?:name?' do
+  if name = params['name']
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
 end
